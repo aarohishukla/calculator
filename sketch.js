82835c89f42264f88e8dae2ca0f9c2632bf3441e
aarohishukla <85484327+aarohishukla@users.noreply.github.com>
2021-06-07 15:11:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,56 +1,44 @@
-var n1,n2;
-var b1,b2,b3,b4;
+var btn_red
+var btn_green
+var r = 0;
+var g = 0;
+var b = 0;
 
 function setup() {
   
   createCanvas(400, 400);
-  
-  num1 = createInput();
-  num1.position(5, 60);
-  
-  num2 = createInput();
-  num2.position(200, 60);
-  
-  b1 = createButton("ADD");
-  b1.position(10, 200);
-  b1.mousePressed(add);
-  
-  b2 = createButton("Multiply");
-  b2.position(100, 200);
-  b2.mousePressed(mul);
+  btn_red = createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
 
-  b3 = createButton("Subtract");
-  b3.position(200,200);
-  b3.mousePressed(sub);
-
-  b4 = createButton("Divide");
-  b4.position(300,200);
-  b4.mousePressed(division);
+  btn_green = createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
   
   
-  textAlign(CENTER);
-  textSize(15);
 }
 
 function draw() {
-  background(200,10,200)
-  text("Number 1",70,50)
-  text("Number 2",270,50)
-  
-  n1 = parseInt(num1.value());
-  n2 = parseInt(num2.value());
+  background(r,b,g);
+
 
 } 
 
-function add(){
-  console.log(n1+n2);
+function red_bg() {
+  r = 255;
+  b = 0;
+  g = 0;
+
+
+
 }
-function sub(){
-  console.log(n1-n2);
-}
-function mul(){
-  console.log(n1*n2);
-}
-function division(){
-  console.log(n1/n2);
+
+
+
+function green_bg() {
+  r = 0; 
+  b = 255;
+  g = 0;
+
+
 }
